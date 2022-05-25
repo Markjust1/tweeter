00d4aff4fe81eccb5f587de02fb722ad1108551a
Artem Novikov <novikov711@gmail.com>
2022-05-25 00:48:30 +0000

    created the page with the form and tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+// const print = () => {
+//   console.log('This is printed inside the text-form');
+//   console.log(this);
+// }
+
+$(document).ready(function() {
+  console.log('document is loaded');
+  let numberOfCharactersLeft = $('.counter'); // targeting number of characters left
+
+  $('#tweet-text').on('keyup', function() {
+    let characterCount = 140 - (this.value.length); // number of input characters 
+    console.log(numberOfCharactersLeft.html());
+    numberOfCharactersLeft.html(characterCount);
+    if (numberOfCharactersLeft.html() < 0) {
+      $('.counter').css( "color", "red" )
+    } else {
+      $('.counter').css( "color", "#545149" )
+    }
+  })
+});

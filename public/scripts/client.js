b3a532fd776e8a517a9bcceef2edee4f21d49026
Artem Novikov <novikov711@gmail.com>
2022-05-26 22:59:02 +0000

    fix the bug with the form output

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,14 +5,11 @@
  */
 // Test / driver code (temporary). Eventually will get this from the server.
 
-///////////////  DATABASE  //////////////////
-
 
 ///////////////  APPENDING FUNCTION  //////////////////
 
 const createTweetElement = function(item) {
   const safeInput = $('<div class="user-text">').text(item.content.text);
-  //console.log(item.content.text);
   safeInput.addClass("tweet-text");
   const $tweet = $(`
     <article class="tweet-container">
@@ -22,8 +19,8 @@ const createTweetElement = function(item) {
           <p class="name">${item.user.name}</p>
         </div>
         <div class="handle">${item.user.handle}</div>
-      </div>`)
-      const footer = $(`
+      </div>`);
+  const footer = $(`
       <div class="tweet-footer">
       <div>${timeago.format(item.created_at)}</div>
       <ul class="icon-list">
@@ -34,12 +31,12 @@ const createTweetElement = function(item) {
     </div>
     </article>
       `);
-      
-    $tweet.append(safeInput);
-    $tweet.append(footer);
-    $('.tweets').prepend($tweet);
-  
-}
+
+  $tweet.append(safeInput);
+  $tweet.append(footer);
+  $('.tweets').prepend($tweet);
+
+};
 
 //////////////   LOOPING THROUGH ITEMS IN DATABASE ///////////////////
 
@@ -49,11 +46,13 @@ const renderTweets = (data) => {
   }
 };
 
+////////////   MESSAGE SUBMISSION //////////////
 
 $(() => {
   $('#submit-form').on('submit', (event) => {
     event.preventDefault();
     let userMessage = $('#tweet-text').val();
+    // form validation
     error();
     if (userMessage.length > 140) {
       error2();
@@ -61,56 +60,60 @@ $(() => {
     }
     const str = $('#submit-form').serialize();
     $.ajax({
-      url:'/tweets',
+      url: '/tweets',
       method: 'POST',
       data: str,
       success: () => {
         loadTweets(true);
+        // resetting form after submission, and keeping form text if over 140 characters
         if (userMessage.length < 140) {
           userMessage = $('#tweet-text').val("");
         }
+        $('#tweet-text').val("");
         $('.counter').val('140');
       }
-    })
-  })
+    });
+  });
+
+  //////   rendering tweets ///////
 
   const loadTweets = (getLast) => {
     $.get('/tweets')
       .then(posts => {
         if (getLast) {
-            const lastTweet = posts[posts.length -1];
-            renderTweets([lastTweet]);
+          const lastTweet = posts[posts.length - 1];
+          renderTweets([lastTweet]);
         } else {
           renderTweets(posts);
         }
-      })
+      });
   };
   loadTweets();
 });
 
+////// FORM VALIDATION ERROR MESSAGES ///////
 
 const error = () => {
   let userMessage = $('#tweet-text').val();
   if (userMessage.length === 0 || userMessage === null) {
     $('.error').val('The field is empty');
-    $('.error').css( "display", "inline-block" );
+    $('.error').css("display", "inline-block");
     document.addEventListener('keydown', () => {
-      $('.error').css( "display", "none" );
-    })
+      $('.error').css("display", "none");
+    });
     return;
   }
   return;
-}
+};
 
 const error2 = () => {
   let userMessage = $('#tweet-text').val();
   if (userMessage.length > 140) {
     $('.error2').val('You have exceeded 140 characters');
-    $('.error2').css( "display", "inline-block" );
+    $('.error2').css("display", "inline-block");
     document.addEventListener('keydown', () => {
-      $('.error2').css( "display", "none" );
-    })
-    //$('#tweet-text').val('');
+      $('.error2').css("display", "none");
+    });
     return;
   }
-}
+};
